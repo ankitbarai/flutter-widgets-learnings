6bd5c7c9c2ffeb8e9d86c809efba2fc84529fbc0
Ankit Barai <ankitbarai2023@gmail.com>
2026-01-19 23:08:16 +0530

    Initial Flutter project

---
+++ lib/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+        titleTextStyle: TextStyle(color: Colors.white,),
+
+      ),
+      body:  Center(
+        child: Container(
+          height: 100,
+          width: 200,
+          color: Colors.black,
+          margin: EdgeInsets.symmetric(horizontal: 30),
+        ),
+      )
+    );
+  }
+}
